e48e128b43345c470ea20f41f30518036ec9184c
云上的云 <liu_yu_quan@foxmail.com>
2024-02-22 16:33:27 +0800

    :beers: 添加用户控制器

---
+++ src/main/java/indi/ly/crush/controller/LoginController.java
@@ -0,0 +1,36 @@
+package indi.ly.crush.controller;
+
+import indi.ly.crush.model.dto.UserDTO;
+import indi.ly.crush.model.entity.User;
+import indi.ly.crush.model.from.UserCredentials;
+import indi.ly.crush.response.ResponseResult;
+import indi.ly.crush.service.IUserService;
+import indi.ly.crush.util.support.BaseSpringBeanUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <h2>登录控制器</h2>
+ *
+ * @since 1.0
+ * @author 云上的云
+ * @formatter:off
+ */
+@RestController
+@RequestMapping(value = "/api/")
+public class LoginController {
+
+    private final IUserService userServiceImpl;
+
+    public LoginController(IUserService userServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
+    }
+
+    @GetMapping(value = "/v1/login")
+    public ResponseResult<UserDTO> goToLogin(UserCredentials userCredentials) {
+        User user = this.userServiceImpl.login(userCredentials);
+        UserDTO userDTO = BaseSpringBeanUtil.shallowCopyObject(user, new UserDTO(), null, null);
+        return ResponseResult.ok(userDTO).message("登录成功");
+    }
+}
