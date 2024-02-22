186f825ea48d42d03def0b0028cb7a6218284eaa
云上的云 <liu_yu_quan@foxmail.com>
2024-02-22 09:21:32 +0800

    :beers: 添加枚举

---
+++ src/main/java/indi/ly/crush/enums/Gender.java
@@ -0,0 +1,29 @@
+package indi.ly.crush.enums;
+
+/**
+ * <h2>性别</h2>
+ *
+ * @since 1.0
+ * @author 云上的云
+ * @formatter:off
+ */
+public enum Gender {
+	/**
+	 * <p>
+	 *     性别之一: 女.
+	 * </p>
+	 */
+	FEMALE,
+	/**
+	 * <p>
+	 *     性别之一: 男.
+	 * </p>
+	 */
+	MALE,
+	/**
+	 * <p>
+	 *     性别之一: 私密.
+	 * </p>
+	 */
+	UNKNOWN
+}
