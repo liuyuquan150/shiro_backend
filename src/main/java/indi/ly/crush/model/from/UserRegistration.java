87e6c8a219eb28801e3058e8902c565af0eeb694
云上的云 <liu_yu_quan@foxmail.com>
2024-02-22 20:20:45 +0800

    :art: 可忽略

--- src/main/java/indi/ly/crush/model/from/UserRegistration.java
+++ src/main/java/indi/ly/crush/model/from/UserRegistration.java
@@ -25,6 +25,14 @@ public class UserRegistration
      */
     private String password;
 
+    public UserRegistration() {
+    }
+
+    public UserRegistration(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
