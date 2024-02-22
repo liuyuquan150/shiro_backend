0817663934c4e7913af1c87e6b623e56b1785a9a
云上的云 <liu_yu_quan@foxmail.com>
2024-02-22 16:35:18 +0800

    :beers: 用户存储库 IUserRepository 添加 Set<String> findRolesByUsername(@Param("username") String username); 方法

--- src/main/java/indi/ly/crush/repository/IUserRepository.java
+++ src/main/java/indi/ly/crush/repository/IUserRepository.java
@@ -2,6 +2,10 @@ package indi.ly.crush.repository;
 
 import indi.ly.crush.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Set;
 
 /**
  * <h2>用户存储库</h2>
@@ -11,5 +15,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @formatter:off
  */
 public interface IUserRepository
-		extends JpaRepository<User, String> {
+        extends JpaRepository<User, String> {
+    /**
+     * <p>
+     *     使用原生 {@code SQL} 根据给定的用户名(<em>username</em>)查找与之关联的所有角色名称.
+     * </p>
+     *
+     * @param username 用户名, 用于查找与之关联的角色.
+     * @return 一个包含角色名称的集合. 如果用户不存在或没有关联的角色, 返回空集合.
+     */
+    @Query(value = " SELECT r.name FROM t_role r JOIN t_user_role ur ON r.id = ur.role_id JOIN t_user u ON ur.user_id = u.id WHERE u.username = :username", nativeQuery = true)
+    Set<String> findRolesByUsername(@Param("username") String username);
 }
