7fbc311471183473d436022cf888db419ac9380c
云上的云 <liu_yu_quan@foxmail.com>
2024-02-22 17:39:47 +0800

    :beers: 添加用户注册表单类 UserRegistration

---
+++ src/main/java/indi/ly/crush/model/from/UserRegistration.java
@@ -0,0 +1,43 @@
+package indi.ly.crush.model.from;
+
+import indi.ly.crush.model.entity.User;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * <h2>用户注册</h2>
+ *
+ * @since 1.0
+ * @author 云上的云
+ * @formatter:off
+ */
+public class UserRegistration
+        implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -6849794470754667710L;
+    /**
+     * @see User#getUsername() User#username
+     */
+    private String username;
+    /**
+     * @see User#getPassword() User#password
+     */
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
